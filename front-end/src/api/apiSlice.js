73f5e35c42fd80661fc4ff05163f4f4bf2239178
Bolori <109837376+Mhboolor@users.noreply.github.com>
2023-08-28 07:30:49 +0330

    feat : config store & rtk query

---
+++ front-end/src/api/apiSlice.js
@@ -0,0 +1,7 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000/" }),
+  endpoints: (builder) => ({}),
+});
