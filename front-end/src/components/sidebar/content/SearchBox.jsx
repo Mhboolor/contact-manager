02c30eedd65d386ab04e7966d1aceb18beb202db
Mhboolor <mhstare81@gmail.com>
2023-09-01 09:01:10 +0330

    fix : fix logo style & link this

--- front-end/src/components/sidebar/content/SearchBox.jsx
+++ front-end/src/components/sidebar/content/SearchBox.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { MdSearch } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 function SearchBox() {
   return (
     <div className="flex flex-col gap-5">
-      <div className="flex items-center text-white">
+      <Link to={"/"} className="flex items-center gap-1 text-white font-semibold">
         <img src="./images/logo.png" alt="Logo_Image" width={30} height={30}/>
         <p>بولوری</p>
-      </div>
+      </Link>
       <div className="flex text-gray-500 border border-gray-500 bg-gray-1 rounded-md">
         <input type="text" className="outline-none bg-transparent pr-2 text-white placeholder:text-gray-500 text-sm" placeholder="جستجو ..."/>
         <button className="text-xl px-2 py-[5px]">
