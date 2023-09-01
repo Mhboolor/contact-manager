0b38afa08a8dd5a1df7119892ebeb1f7dcd4b22a
Mhboolor <mhstare81@gmail.com>
2023-09-01 09:37:29 +0330

    feat : remove icon & add button for delete & edit

--- front-end/src/components/Contact.jsx
+++ front-end/src/components/Contact.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { TbDots } from "react-icons/tb";
 import { Link } from "react-router-dom";
 
 function Contact({ grid, id, name, last, image, phone, email, relation }) {
+
   return (
     <div
-      className={`text-zinc-300 relative gap-4 rounded-md p-2 duration-150 ease-in-out hover:bg-gray-2 flex flex-col items-center hover:text-white ${
+      className={`text-zinc-300 gap-4 rounded-md p-2 duration-150 ease-in-out hover:bg-gray-2 flex flex-col items-center ${
         grid ? "sm:flex-col" : "sm:flex-row sm:text-start"
       }`}
     >
       <Link
         to={`/user/${id}`}
-        className={`text-zinc-300 flex-1 gap-4 flex flex-col items-center text-center w-full h-full ${
+        className={`text-zinc-300 flex-1 gap-4 flex flex-col items-center text-center w-full h-full hover:text-white ${
           grid ? "sm:flex-col" : "sm:flex-row sm:text-start"
         }`}
       >
@@ -31,13 +31,10 @@ function Contact({ grid, id, name, last, image, phone, email, relation }) {
           <p>{email}</p>
         </div>
       </Link>
-      <button
-        className={`p-1 hover:bg-gray-1 rounded-md duration-150 ease-in-out z-20 static ${
-          grid ? "sm:static" : "sm:absolute left-4"
-        }`}
-      >
-        <TbDots />
-      </button>
+        <div className="flex items-center justify-center text-center rounded bg-black border border-gray-400 divide-x divide-x-reverse">
+          <button value={id} onClick={e => console.log(e.target.value)} className="w-full px-2 py-1 hover:bg-gray-2 duration-150 ease-in-out hover:text-white">ویرایش</button>
+          <button value={id} onClick={e => console.log(e.target.value)} className="w-full px-2 py-1 hover:bg-gray-2 duration-150 ease-in-out hover:text-white">حذف</button>
+        </div>
     </div>
   );
 }
--- front-end/src/components/sidebar/content/SearchBox.jsx
+++ front-end/src/components/sidebar/content/SearchBox.jsx
@@ -5,12 +5,19 @@ import { Link } from "react-router-dom";
 function SearchBox() {
   return (
     <div className="flex flex-col gap-5">
-      <Link to={"/"} className="flex items-center gap-1 text-white font-semibold">
-        <img src="./images/logo.png" alt="Logo_Image" width={30} height={30}/>
+      <Link
+        to={"/"}
+        className="flex items-center gap-1 text-white font-semibold"
+      >
+        <img src="./images/logo.png" alt="Logo_Image" width={30} height={30} />
         <p>بولوری</p>
       </Link>
       <div className="flex text-gray-500 border border-gray-500 bg-gray-1 rounded-md">
-        <input type="text" className="outline-none bg-transparent pr-2 text-white placeholder:text-gray-500 text-sm" placeholder="جستجو ..."/>
+        <input
+          type="text"
+          className="outline-none bg-transparent pr-2 text-white placeholder:text-gray-500 text-sm"
+          placeholder="جستجو ..."
+        />
         <button className="text-xl px-2 py-[5px]">
           <MdSearch />
         </button>
